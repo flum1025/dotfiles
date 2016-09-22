92fe2b722e5355d82117be2557885f2119be00ce
flum1025 <flum.1025@gmail.com>
2016-09-23 02:38:59 +0900

    vim

--- lnfiles.sh
+++ lnfiles.sh
@@ -1,3 +1,7 @@
 ln -s -i ~/dotfiles/vimrc/.vimrc ~
-ln -s -i ~/dotfiles/tmux/.tmux.cong ~
+echo "alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim \"$@\"'" >> ~/.bash_profile
+echo "alias vim='env_LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim \"$@\"'" >> ~/.bash_profile
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s -i ~/dotfiles/tmux/.tmux.conf ~
+echo 'launch tmux and prefix+I'
