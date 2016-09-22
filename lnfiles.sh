4ceac381416d7c09a59e4f44e0d9145909f58f4a
flum1025 <flum.1025@gmail.com>
2016-09-23 02:45:25 +0900

    update

--- lnfiles.sh
+++ lnfiles.sh
@@ -5,3 +5,5 @@ echo "alias vim='env_LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vi
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s -i ~/dotfiles/tmux/.tmux.conf ~
 echo 'launch tmux and prefix+I'
+
+source ~/.bash_profile
