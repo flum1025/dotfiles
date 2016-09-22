368209cdfeff8e99c5c4789fbe64720f05b97f12
flum1025 <flum.1025@gmail.com>
2016-09-23 02:51:06 +0900

    update

--- lnfiles.sh
+++ lnfiles.sh
@@ -1,6 +1,6 @@
 ln -s -i ~/dotfiles/vimrc/.vimrc ~
-echo "alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim \"$@\"'" >> ~/.bash_profile
-echo "alias vim='env_LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim \"$@\"'" >> ~/.bash_profile
+echo "alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim \"\$@\"'" >> ~/.bash_profile
+echo "alias vim='env_LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim \"\$@\"'" >> ~/.bash_profile
 
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
