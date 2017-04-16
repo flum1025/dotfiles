85abb07c650fef177d4608df6a8dd6ce63a86750
flum1025 <flum.1025@gmail.com>
2017-04-17 04:33:51 +0900

    add quick install for mac

---
+++ qinstall-mac.sh
@@ -0,0 +1,3 @@
+#curl -fsSL https://raw.githubusercontent.com/flum1025/dotfiles/master/qinstall-mac.sh | sh
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"git clone https://github.com/flum1025/dotfiles.git ~/dotfiles
+~/dotfiles/install-mac.sh
