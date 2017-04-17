3e0c2dcde1bb306135fe595ce50c74709b0e06b9
flum1025 <flum.1025@gmail.com>
2017-04-17 23:37:53 +0900

    auto install for mac

--- install-mac.sh
+++ install-mac.sh
@@ -2,7 +2,7 @@ if ! which brew >/dev/null 2>&1; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install ctags tmux reattach-to-user-namespace
+brew install ctags tmux reattach-to-user-namespace zsh jq gawk
 source ~/dotfiles/lnfiles.sh
 
 cat <<EOF >> ~/dotfiles/tmux/.tmux.conf
