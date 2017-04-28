46edf7d056290085b096df652ff80908a7d3de3c
flum1025 <flum.1025@gmail.com>
2017-04-28 18:53:00 +0900

    fix

--- install-linux.sh
+++ install-linux.sh
@@ -1,4 +1,4 @@
 apt update
 apt install -y vim-gnome tmux git ctags build-essential curl zsh gawk jq
-source ~/dotfiles/lnfiles.sh
-source ~/dotfiles/install-common.sh
+. ~/dotfiles/lnfiles.sh
+. ~/dotfiles/install-common.sh
