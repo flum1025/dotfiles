02b5f65f820a9be0e7a153ab7d89efaf0484a585
Yuki Suwa <flum.1025@gmail.com>
2020-08-28 20:08:44 +0900

    Update alias.zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,11 +2,5 @@
 alias l='ls'
 alias la='ls -aG'
 alias ll='ls -lG'
-alias count_rb='wc -l `find ./ -name "*.rb"`'
 alias lfind='mdfind -onlyin ./ '
 alias cssh='~/dotfiles/tmux/tmux-cssh.sh'
-
-#関数
-file_count(){
-  wc -l `find ./ -name "$1"`
-}
