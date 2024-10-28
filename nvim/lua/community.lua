798ba1ea38fff11a78d63645e314324ecc6a11c7
flum1025 <flum.1025@gmail.com>
2024-10-28 14:54:38 +0900

    blame-nivm

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,5 +5,6 @@ return {
   { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.git.openingh-nvim" },
+  { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.editing-support.copilotchat-nvim" },
 }
