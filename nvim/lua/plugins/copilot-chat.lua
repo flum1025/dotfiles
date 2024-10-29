0b026b7e3f41eaf22d480601b95bf4f0638976d8
flum1025 <flum.1025@gmail.com>
2024-10-29 19:44:53 +0900

    Add CopilotChat.nvim plugin with gitcommit autocmd

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,24 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "gitcommit",
+      once = true,
+      callback = function()
+        vim.notify("callback", vim.log.levels.INFO)
+        local chat = require "CopilotChat"
+        chat.open()
+        chat.ask(
+          "Generate a concise and meaningful commit message title based on the changes made. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.",
+          {
+            selection = function(source) return require("CopilotChat.select").gitdiff(source, true) end,
+            callback = function(response)
+              chat.close()
+              vim.api.nvim_buf_set_lines(0, 0, 1, false, { response })
+            end,
+          }
+        )
+      end,
+    })
+  end,
+}
