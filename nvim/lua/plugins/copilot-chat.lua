9debb2e9d9531790726208fa81d2f0e5f791b90c
flum1025 <flum.1025@gmail.com>
2024-11-20 18:39:02 +0900

    Check for empty commit message before generating commit message

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -5,18 +5,22 @@ return {
       pattern = "gitcommit",
       once = true,
       callback = function()
-        local chat = require "CopilotChat"
-        chat.open()
-        chat.ask(
-          "Generate a concise and meaningful commit message title based on the changes made. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.",
-          {
-            selection = function(source) return require("CopilotChat.select").gitdiff(source, true) end,
-            callback = function(response)
-              chat.close()
-              vim.api.nvim_buf_set_lines(0, 0, 1, false, { response })
-            end,
-          }
-        )
+        local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+
+        if first_line == nil or first_line == "" then
+          local chat = require "CopilotChat"
+          chat.open()
+          chat.ask(
+            "Generate a concise and meaningful commit message title based on the changes made. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.",
+            {
+              selection = function(source) return require("CopilotChat.select").gitdiff(source, true) end,
+              callback = function(response)
+                chat.close()
+                vim.api.nvim_buf_set_lines(0, 0, 1, false, { response })
+              end,
+            }
+          )
+        end
       end,
     })
   end,
