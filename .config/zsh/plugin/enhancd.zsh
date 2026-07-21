9635e9b3fe8ac4934b832b1df4caaf1bac920545
flum1025 <flum.1025@gmail.com>
2026-07-22 04:10:52 +0900

    Fix cd

--- .config/zsh/plugin/enhancd.zsh
+++ .config/zsh/plugin/enhancd.zsh
@@ -1,3 +1,6 @@
-ENHANCD_DISABLE_DOT=1
-ENHANCD_DISABLE_HOME=1
+# `cd ..` / `cd .` / `cd`(引数なし)で fzf 選択画面を出さず、
+# builtin cd と同じ挙動にする(変数名は enhancd 2.5.x のもの)
+ENHANCD_ENABLE_DOUBLE_DOT=false
+ENHANCD_ENABLE_SINGLE_DOT=false
+ENHANCD_ENABLE_HOME=false
 ENHANCD_HYPHEN_NUM=20
